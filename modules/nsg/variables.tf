85ac23ab34a7b2b79d42ebfaa3c721ce4fe88536
Sampurnanand dwivedi <51002284+sampurnananddwivedi@users.noreply.github.com>
2025-12-17 22:49:08 +0530

    Create variables.tf

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "nsgs" {
+  description = "Map of NSGs and their security rules"
+  type = map(object({
+    name = string
+    rules = list(object({
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    }))
+  }))
+}
